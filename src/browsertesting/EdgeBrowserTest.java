238b567d535df5072f065d6b3678cc048ab19449
vaibhdoshi <vaibhdoshi@gmail.com>
2022-11-15 13:36:45 +0000

    project-2-orangehrmlive

---
+++ src/browsertesting/EdgeBrowserTest.java
@@ -0,0 +1,41 @@
+package browsertesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class EdgeBrowserTest {
+    public static void main(String[] args) {
+
+        String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+        System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        //Launch the URL
+        driver.get(baseUrl);
+        //We give implicit time to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        //Get the title of the page
+        String title = driver.getTitle();
+        System.out.println("Page title is : " + title);
+        //get current URL
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        //get page source
+        System.out.println("Page source : " + driver.getPageSource());
+        //Find the email field element and send the email
+        WebElement emailField = driver.findElement(By.name("username"));
+        //type email to email field
+        emailField.sendKeys("Admin");
+        //Find the password field element
+        WebElement passwordField = driver.findElement(By.name("password"));
+        //type password to password field
+        passwordField.sendKeys("admin123");
+        //close the scanner
+        driver.quit();
+
+
+    }
+
+}
